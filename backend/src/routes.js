34a8489a37b0b85120020ce5ae74c448635cb76d
Ricardo Amorim <ricardoinamorim@gmail.com>
2023-05-11 00:42:25 -0300

    Adicionado rotas e contrutores para usuer e login

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,6 +4,8 @@ const routes = express.Router();
 const CourseController = require('./controllers/CourseController');
 const SubjectController = require('./controllers/SubjectController');
 const ClassController = require('./controllers/ClassController');
+const UserController = require('./controllers/UserController');
+const LoginController = require('./controllers/LoginController');
 
 // Course Routes
 routes.get('/course', CourseController.read);
@@ -25,5 +27,15 @@ routes.get('/class/:id', ClassController.readId);
 routes.post('/class', ClassController.create);
 routes.put('/class/:id', ClassController.update);
 routes.delete('/class/:id', ClassController.delete);
+
+// User Routes
+routes.get('/user', UserController.read);
+routes.get('/user/:id', UserController.readId);
+routes.post('/user', UserController.create);
+routes.put('/user/:id', UserController.update);
+routes.delete('/user/:id', UserController.delete);
+
+// Login Routes
+routes.post('/login', LoginController.login);
 
 module.exports = routes;
